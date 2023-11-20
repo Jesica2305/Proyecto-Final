32a7836038b8df544e061af5ca8b99e0fb2ed346
ManesCruz <manuele.cruzp@konradlorenz.edu.co>
2023-11-20 13:30:23 -0500

    correccion

---
+++ src/presentacion/VentanaPrincipal.java
@@ -0,0 +1,9 @@
+package presentacion;
+
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JPanel pnlPrincipal;
+    private JLabel lblRecomendaciones;
+    private JList list1;
+}
