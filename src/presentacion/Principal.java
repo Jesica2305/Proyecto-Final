37eb0343600fe7e8eb8150082d39eeac72c903c7
danielaaaa1111 <142046996+danielaaaa1111@users.noreply.github.com>
2023-11-24 11:57:32 -0500

    Principal.java

--- src/presentacion/Principal.java
+++ src/presentacion/Principal.java
@@ -4,8 +4,11 @@ import logicaNegocio.Juego;
 import logicaNegocio.Tablero;
 
 public class Principal {
+    // Creacion de la ventana principal del juego 
     public static void main(String[] args) {
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal(new Juego(new Tablero(7)));
+
+        //Inicializacion de la ventana principal
         ventanaPrincipal.inicializarVentana();
     }
 }
