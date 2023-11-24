c44bbd1035306a7758c4b835f587c95c418feffc
Jesica2305 <142047118+Jesica2305@users.noreply.github.com>
2023-11-24 12:24:17 -0500

    Principal.java

--- src/presentacion/Principal.java
+++ src/presentacion/Principal.java
@@ -2,9 +2,16 @@ package presentacion;
 
 import logicaNegocio.Juego;
 import logicaNegocio.Tablero;
-
+/**
+*Clase Pricipal 
+*/
 public class Principal {
-    // Creacion de la ventana principal del juego 
+    /**
+    *Creacion de la ventana principal del juego 
+    *Primer método en ser ejecutado
+    *@param args Argumentos que recibe por consola
+    *Complejidad temporal:O(1) complejidad constante
+    */
     public static void main(String[] args) {
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal(new Juego(new Tablero(7)));
 
